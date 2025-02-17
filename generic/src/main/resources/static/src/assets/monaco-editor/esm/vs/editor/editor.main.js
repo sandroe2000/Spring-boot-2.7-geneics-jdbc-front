2b7fc4dd4c1c7f1cf085e8bde5b54837debce9a7
Sandro Cesário Di Espindula <sandroe2000@gmail.com>
2025-02-17 18:40:37 -0300

    clean static files

--- generic/src/main/resources/static/src/assets/monaco-editor/esm/vs/editor/editor.main.js
+++
@@ -1,7 +0,0 @@
-import '../basic-languages/monaco.contribution.js';
-import '../language/css/monaco.contribution.js';
-import '../language/html/monaco.contribution.js';
-import '../language/json/monaco.contribution.js';
-import '../language/typescript/monaco.contribution.js';
-
-export * from './edcore.main.js';
